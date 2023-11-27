c91fff9e4e736cbf07483e81108dd867480f8f81
jisoleil <janggh1012@naver.com>
2023-11-27 15:50:04 +0900

    mywork_accodian-HTML,CSS,JS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 
+//plus btn 클릭 이벤트
 $('.skills__plus__btn').on('click', function(event) {
     //배경 흐리게
     $(".shadow").show();
@@ -11,7 +12,7 @@ $('.skills__plus__btn').on('click', function(event) {
     } else if ($(this).hasClass('be__btn')) {
         $('.skills__be__info').show();
     }
-    
+
     $('.close__btn').on('click',function(event){
         $('.skills__fe__info').hide();
         $('.skills__be__info').hide();
@@ -21,11 +22,34 @@ $('.skills__plus__btn').on('click', function(event) {
     })
 });
 
+let div_Img_Slide_down=()=>{
+    $('.project1__metadata__box').hide()
+    $('.project2__metadata__box').hide()
+    $('.project__img1').hide();
+    $('.project__img2').hide();
+}
 
-$(document).on('click','.slide__down',
-function(){
-    $(this).hide();
-    $('.slide__up').removeClass('.display__none');
+
+
+$('.project__img2').hide();
+$('.project1__metadata__box').show();
+$(document).on('click','.slide__down',function(event){
+    //slide__down 버튼 click 하면 slide__down 사라지고, slide__up 버튼 보이기
+    if($(this).hasClass('project1')){
+        div_Img_Slide_down();
+        $('.project1__metadata__box').slideToggle();
+        $('.project__img1').show();
+
+    } else if ($(this).hasClass('project2')){
+        div_Img_Slide_down();
+        $('.project2__metadata__box').slideToggle();
+        $('.project__img2').show();
+    }   
+
+    
+
+
+
 });
 
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용(대강으로 주석달기)
